16c68033705daf86c82845a6f5131284dad9f77c
benjamin <benjamin.mcbrayer@gmail.com>
2018-06-21 09:56:37 -0400

    oneplayer login to gameboard with controller

--- src/main/java/com/gc/scrumble/oops/LogicController.java
+++ src/main/java/com/gc/scrumble/oops/LogicController.java
@@ -25,66 +25,107 @@ public class LogicController {
 
 	@Autowired
 	UsersRepository uP;
-	
+
 	@Autowired
 	RootwordsRepository rP;
 
-	@RequestMapping("/dummylogin")
+	@RequestMapping("/oneplayerlogin")
 	public String login() {
-		return "dummylogin";
+		return "oneplayerlogin";
 	}
 
 	@RequestMapping("add1")
 	public ModelAndView addPlayerOne(@RequestParam("newusername1") String username,
 			@RequestParam("newpword1") String pword, HttpSession session, Model model) {
-		User user = new User(username, pword);
-		uP.save(user);
-		model.addAttribute("newusername1", username);
-		return new ModelAndView("redirect:/dummylogin", "thankyou1", "Thank you, " + username + ".");
+		User newuser = new User(username, pword);
+		Optional<User> user = uP.findByUsername(username);
+		if (user.isPresent()) {
+			String message = "User name already exists. Please choose another or log in below.";
+			ModelAndView mv = new ModelAndView("oneplayerlogin", "alreadyexists1", message);
+			return mv;
+		} else {
+			uP.save(newuser);
+			model.addAttribute("newusername1", username);
+			String message = "Thank you, " + username + ".";
+			return new ModelAndView("secondlogin", "thankyou1", message);
+		}
 	}
 
-	@RequestMapping("add2")
-	public ModelAndView addPlayerTwo(@RequestParam("newusername2") String username,
-			@RequestParam("newpword2") String pword, HttpSession session, Model model) {
-		User user = new User(username, pword);
-		uP.save(user);
-		model.addAttribute("newusername2", username);
-		return new ModelAndView("redirect:/dummylogin", "thankyou2", "Thank you, " + username + ".");
-	}
+//	@RequestMapping("add2")
+//	public ModelAndView addPlayerTwo(@RequestParam("newusername2") String username,
+//			@RequestParam("newpword2") String pword, HttpSession session, Model model) {
+//		User newuser = new User(username, pword);
+//		Optional<User> user = uP.findByUsername(username);
+//		if (user.isPresent()) {
+//			String message = "User name already exists. Please choose another or log in below.";
+//			ModelAndView mv = new ModelAndView("dummylogin", "alreadyexists2", message);
+//			return mv;
+//		} else {
+//			uP.save(newuser);
+//			model.addAttribute("newusername2", username);
+//			String message = "Thank you, " + username;
+//			return new ModelAndView("dummylogin", "thankyou2", message);
+//		}
+//	}
 
 	@RequestMapping("login1")
-	public ModelAndView addUser(@RequestParam("username1") String username, @RequestParam("pword1") String pword,
+	public ModelAndView userLogin1(@RequestParam("username1") String username, @RequestParam("pword1") String pword,
 			HttpSession session, Model model) {
 		Optional<User> user = uP.findByUsername(username);
 		if (user.isPresent() && user.get().getPword().equals(pword)) {
-			ModelAndView mv = new ModelAndView("dummyindex", "welcome1", "Thank you, " + username);
+			ModelAndView mv = new ModelAndView("oneplayerlogin", "welcome1", "Welcome to Scrumble, " + username + "!");
 			model.addAttribute("username1", username);
 			return mv;
 		}
-		return new ModelAndView("dummyindex", "failure1",
+		return new ModelAndView("oneplayerlogin", "failure1",
 				"User name and password do not match. Please check your credentials, fellow Scrumbler.");
 	}
-
-	@RequestMapping("/login2")
-	public ModelAndView login(@RequestParam("username2") String username, @RequestParam("pword2") String pword,
+	
+	@RequestMapping("secondlogin1")
+	public ModelAndView secondUserLogin1(@RequestParam("username1") String username, @RequestParam("pword1") String pword,
 			HttpSession session, Model model) {
 		Optional<User> user = uP.findByUsername(username);
 		if (user.isPresent() && user.get().getPword().equals(pword)) {
-			ModelAndView mv = new ModelAndView("dummyindex", "welcome2", "Thank you, " + username);
-			model.addAttribute("username2", username);
+			ModelAndView mv = new ModelAndView("secondlogin", "secondwelcome1", "Welcome to Scrumble, " + username + "!");
+			model.addAttribute("username1", username);
 			return mv;
 		}
-		return new ModelAndView("dummyindex", "failure2",
+		return new ModelAndView("secondlogin", "secondfailure1",
 				"User name and password do not match. Please check your credentials, fellow Scrumbler.");
 	}
 
+//	@RequestMapping("/login2")
+//	public ModelAndView login(@RequestParam("username2") String username, @RequestParam("pword2") String pword,
+//			HttpSession session, Model model) {
+//		Optional<User> user = uP.findByUsername(username);
+//		if (user.isPresent() && user.get().getPword().equals(pword)) {
+//			ModelAndView mv = new ModelAndView("dummylogin", "welcome2", "Thank you, " + username);
+//			model.addAttribute("username2", username);
+//			return mv;
+//		}
+//		return new ModelAndView("dummylogin", "failure2",
+//				"User name and password do not match. Please check your credentials, fellow Scrumbler.");
+//	}
+
 	// This method starts immediately upon pressing Play button.
-	@RequestMapping("/play")
+	@RequestMapping("play")
 	public ModelAndView play(HttpSession session, Model model) {
 		List<Rootword> rootwordList = new ArrayList<>();
 		rootwordList = rP.findAll();
 		long listLength = rootwordList.size();
-		int randomIndex = (int)(Math.random()*listLength);
+		int randomIndex = (int) (Math.random() * listLength);
+		Rootword rootword = rootwordList.get(randomIndex);
+		ModelAndView mv = new ModelAndView("gameboard", "rootword", rootword);
+		model.addAttribute("rootword", rootword);
+		return mv;
+	}
+	
+	@RequestMapping("secondplay")
+	public ModelAndView secondPlay(HttpSession session, Model model) {
+		List<Rootword> rootwordList = new ArrayList<>();
+		rootwordList = rP.findAll();
+		long listLength = rootwordList.size();
+		int randomIndex = (int) (Math.random() * listLength);
 		Rootword rootword = rootwordList.get(randomIndex);
 		ModelAndView mv = new ModelAndView("gameboard", "rootword", rootword);
 		model.addAttribute("rootword", rootword);
